f8cac344a329d081e8da06e03025ac0e814123c7
= <virginie1@Virginies-MacBook-Pro.local>
2020-02-28 22:19:01 -0800

    attemped (failed) to add read states

--- choosefallacy.js
+++ choosefallacy.js
@@ -1,14 +1,30 @@
+var authread = false;
+var cherryread = false;
+var midread = false;
+
 function chooseAuth(){
     document.querySelector("#fallacypage").style.display = "none";
     document.querySelector("#authpage").style.display = "block";
+    
+    authread = true;
 }
 
 function chooseCherry(){
     document.querySelector("#fallacypage").style.display = "none";
     document.querySelector("#cherrypage").style.display = "block";
+
+    cherryread = true;
 }
 
 function chooseMiddle(){
     document.querySelector("#fallacypage").style.display = "none";
     document.querySelector("#middlegroundpage").style.display = "block";
+
+    midread = true;
+    console.log(midread);
+}
+
+if (midread === true && authread === true && cherryread === true){
+    document.querySelector("fallacyreadpage").style.display = "block";
+    document.querySelector("fallacypage").style.display = "none";
 }
