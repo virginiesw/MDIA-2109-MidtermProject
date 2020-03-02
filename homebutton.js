62c3af9bd88ccdd9bf2cf5795bb2286caa660111
cpark3918 <cindypark31@gmail.com>
2020-03-01 22:01:47 -0800

    added responsive design

--- homebutton.js
+++ homebutton.js
@@ -3,6 +3,7 @@ function backToHome() {
     document.querySelector("#cherrypage").style.display = "none";
     document.querySelector("#authpage").style.display = "none";
     document.querySelector("#fallacypage").style.display = "block";
+    document.querySelector("#quizpage1").style.display = "none";
 
     if (midread === true && cherryread === true && authread === true){
         document.querySelector("#appearonread").style.display = "block";
---
+++ quiz.js
@@ -0,0 +1,68 @@
+var question_state = 1;
+
+function StartQuiz() {
+    document.querySelector("#fallacypage").style.display = "none";
+    document.querySelector("#quizpage1").style.display = "block";
+    document.querySelector("#quizpage_last").style.display = "none";
+}
+
+function NextQuestion1() {
+    document.querySelector("#quizpage1").style.display = "none";
+    document.querySelector("#quizpage2").style.display = "block";
+    question_state = 2;
+}
+
+function NextQuestion2() {
+    document.querySelector("#quizpage2").style.display = "none";
+    document.querySelector("#quizpage3").style.display = "block";
+    question_state = 3;
+}
+
+function NextQuestion3() {
+    document.querySelector("#quizpage3").style.display = "none";
+    document.querySelector("#quizpage4").style.display = "block";
+    question_state = 4;
+}
+
+function NextQuestion4() {
+    document.querySelector("#quizpage4").style.display = "none";
+    document.querySelector("#quizpage5").style.display = "block";
+    question_state = 5;
+}
+
+function NextQuestion5() {
+    document.querySelector("#quizpage5").style.display = "none";
+    document.querySelector("#quizpage6").style.display = "block";
+    question_state = 6;
+}
+
+function QuizResults() {
+    document.querySelector("#quizpage6").style.display = "none";
+    document.querySelector("#quizpage_last").style.display = "block";
+}
+
+//function validateForm() {
+    //var input = document.forms["answer"]["answerfield"].value;
+    //if (input == "") {
+       // alert("You must fill out the answer!");
+        //return false;
+    //}
+//}
+
+function backToPrev(){
+    if (question_state === 2){
+        StartQuiz();
+    } else if (question_state === 3){
+        document.querySelector("#fallacypage").style.display = "none";
+        NextQuestion1();
+    } else if (question_state === 4){
+        document.querySelector("#fallacypage").style.display = "none";
+        NextQuestion2();
+    } else if (question_state === 5){
+        document.querySelector("#fallacypage").style.display = "none";
+        NextQuestion3();
+    } else if (question_state === 6){
+        document.querySelector("#fallacypage").style.display = "none";
+        NextQuestion2();
+    }
+}
